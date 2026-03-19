40414e401bb97e0a27002ea8a8883aa9e51f8103
Katona Márk <katonamark0919@gmail.com>
2026-03-19 11:50:11 +0100

    inpuut

--- kviz.js
+++ kviz.js
@@ -1,15 +1,43 @@
 let urlSearch = window.location.search
 // let url = window.location.href
 
-if(urlSearch.includes("?")){
+if (urlSearch.includes("?")) {
     console.log(urlSearch);
     let kerdes = urlSearch.split("?")[1]
-    let fovaros = kerdes.split("=")[1]
-    console.log(kerdes);
-    console.log(fovaros);
+    // console.log("kerdes: ", kerdes)
+    let kerdesParok = kerdes.split("&")
+    console.log(kerdesParok);
+    let fovaros = ""
+    let vizeses = ""
+    for (let i = 0; i < kerdesParok.length; i++) {
+        console.log(kerdesParok[i]);
+        if (kerdesParok[i].startsWith("fovaros")) {
+            fovaros = kerdesParok[i].split("=")[1] 
 
-let felhasznaloBevitelEredmenye = document.getElementById("felhasználói_input")
-felhasznaloBevitelEredmenye.innerHTML = `
+        } else if (kerdesParok[i].startsWith("vizeses")) {
+            vizeses = kerdesParok[i].split("=")[1]
+            console.log("VIZESÉS", vizeses);
+            
+        }
+
+
+    }
+    let latnivalok = []
+    let latnivalokSzoveg = ""
+    for (let i = 0; i < kerdesParok.length; i++) {
+        if (kerdesParok[i].startsWith("latnivalo")) {
+            latnivalok.push(kerdesParok[i].split("=")[1])
+        }
+    }
+    latnivalokSzoveg = latnivalok.join(", ")
+    console.log(latnivalok);
+    console.log(latnivalokSzoveg);
+
+    let felhasznaloBevitelEredmenye = document.getElementById("felhasználói_input")
+    felhasznaloBevitelEredmenye.innerHTML = `
 <h3>A felhasználó válasza</h3>
-    <p>1. ${fovaros}</p>`
+    <p>1. ${fovaros}</p>
+    <p>2. ${vizeses}</p>
+    <p>3. ${latnivalokSzoveg}</p>
+    `
 }
